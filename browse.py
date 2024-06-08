7e277257ce9990c6aa3ed3e979ba9d4c4a71b911
gueffie <142988365+gueffie@users.noreply.github.com>
2024-06-08 14:14:04 +0200

    Add files via upload

--- browse.py
+++ browse.py
@@ -1,14 +1,12 @@
 import requests
 
 
-#ce code permet de recupérer creer l'url permettant de recuperer des informations
-# de la recherche voiture lorsqu'il
-# est entré dans le browser
+
 
 
 query = input("que Recherchez-Vous ?")
 starts = list(x for x in range(1,100,10))
-for start in starts :
+for start in starts:
     API_KEY = "AIzaSyB9Ybuhj2mimH0AWLweV9ljVA4S_oOCX44"
     SEARCH_ENGINE_ID = "014463886005110263066:h5g73seoq-8"
     url = (f"https://www.googleapis.com/customsearch/v1?key={API_KEY}"
